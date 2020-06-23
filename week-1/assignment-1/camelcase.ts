79aa887883f86ad487473c27fc1b5e849f8e19e3
Andre Macedo <dee.xd@hotmail.com>
2020-06-23 18:43:00 -0300

    Fix assignment week 1

--- week-1/assignment-1/camelcase.ts
+++ week-1/assignment-1/camelcase.ts
@@ -1,13 +1,13 @@
 export default function camelcase(str: string): string[] {
-    if (/^[0-9]/.test(str)) throw new Error('Error: word cannot start with number')
-    if (/\W/g.test(str)) throw new Error('Error: word cannot have special symbols')
+    validateString(str)
 
-    const regexWordStart = /([A-Z]{1}[a-z]{1})/g
-    const regexWordEnd = /([a-z]{1}([A-Z]{1}|[0-9]{1}))/g
-
-    str = str.replace(regexWordStart, (txt: string) => ` ${txt.toLowerCase()}`)
-    str = str.replace(regexWordEnd, (txt: string) => `${txt.charAt(0)} ${txt.charAt(1)}`)
+    str = str.replace(/([A-Z]{1}[a-z]{1})/g, (txt: string) => ` ${txt.toLowerCase()}`)
+    str = str.replace(/([a-z]{1}([A-Z]{1}|[0-9]{1}))/g, (txt: string) => `${txt.charAt(0)} ${txt.charAt(1)}`)
 
     return str.trim().split(' ')
 }
 
+function validateString(str: string): void {
+    if (/^[0-9]/.test(str)) throw new Error('Error: word cannot start with number')
+    if (/\W/g.test(str)) throw new Error('Error: word cannot have special symbols')
+}
